0b726bbfcc0b0b0009e405129796d98c3bc699c9
plusyoursoftech <plusyoursoftech@users.noreply.github.com>
2025-12-27 12:16:51 -0500

    Revie and fixed issue

--- AutoFixerApplication.java
+++ AutoFixerApplication.java
@@ -1,4 +1,4 @@
-package com.plusyoursoftech.AutoFixer;
+package com.plusyoursoftech.autofixer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,9 +7,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan({"com.plusyoursoftech.AutoFixer", "com.plusyoursoftech.af.service", "com.plusyoursoftech.af.controller"})
-@EnableJpaRepositories(basePackages = "com.plusyoursoftech.af.repo")
-@EntityScan(basePackages = "com.plusyoursoftech.af.domain")
+@ComponentScan({"com.plusyoursoftech.autofixer", "com.plusyoursoftech.autofixer.service", "com.plusyoursoftech.autofixer.controller"})
+@EnableJpaRepositories(basePackages = "com.plusyoursoftech.autofixer.repo")
+@EntityScan(basePackages = "com.plusyoursoftech.autofixer.domain")
 public class AutoFixerApplication {
 
 	public static void main(String[] args) {
