c36445182dfef22c4cef7a6f9c92d01deb8be90d
plusyoursoftech <plusyoursoftech@users.noreply.github.com>
2025-12-27 12:52:14 -0500

    good loookin

--- AutoFixerApplication.java
+++ AutoFixerApplication.java
@@ -3,11 +3,9 @@ package com.plusyoursoftech.autofixer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan({"com.plusyoursoftech.autofixer", "com.plusyoursoftech.autofixer.service", "com.plusyoursoftech.autofixer.controller"})
 @EnableJpaRepositories(basePackages = "com.plusyoursoftech.autofixer.repo")
 @EntityScan(basePackages = "com.plusyoursoftech.autofixer.domain")
 public class AutoFixerApplication {
